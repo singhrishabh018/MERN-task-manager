83bb3d905a672c90ebe9b12f7b9189adbc0fc55e
Rishabh Singh <singhrishabh018@gmail.com>
2024-04-16 14:03:34 -0400

    Update taskActions.js

--- frontend/src/redux/actions/taskActions.js
+++ frontend/src/redux/actions/taskActions.js
@@ -1,1 +1,5 @@
+// taskActions.js
+export const addTask = (task) => ({ type: 'ADD_TASK', payload: task });
+export const updateTask = (task) => ({ type: 'UPDATE_TASK', payload: task });
+export const removeTask = (taskId) => ({ type: 'REMOVE_TASK', payload: taskId });
 
