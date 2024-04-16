01f3b372520d90a08c3d9a482247905ab71c1fef
Rishabh Singh <singhrishabh018@gmail.com>
2024-04-16 13:11:15 -0400

    Update app.js
    added real-time features

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,10 @@ const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 
+const server = require('http').createServer(app);
+const io =  require('socket.io')(server);
+
+
 app.use(express.json());
 app.use(cors());
 
@@ -25,6 +29,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "../frontend/build")));
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
 }
+
+io.on('connection', (socket) => {
+    console.log('User connected!');
+
+    // Logic for handling task updates will go here
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected!');
+    });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
