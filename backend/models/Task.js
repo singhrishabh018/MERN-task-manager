67cbe7a87a0cc6b976e3fbf1c17bc3ea96b4116e
Rishabh Singh <singhrishabh018@gmail.com>
2024-04-16 13:38:14 -0400

    Update Task.js

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -10,10 +10,17 @@ const taskSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  collaborators: [ 
+     { type: mongoose.Schema.Types.ObjectId, ref: 'User' } 
+  ], // Array to store IDs of collaborating users
+  priority: {
+    type: String, 
+    enum: ['low', 'medium', 'high'], 
+    default: 'medium' 
+  },
 }, {
   timestamps: true
 });
-
 
 const Task = mongoose.model("Task", taskSchema);
 module.exports = Task;
