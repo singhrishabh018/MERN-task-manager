2250a0b804457825a83d81ec16c6718d25bc3756
Rishabh Singh <singhrishabh018@gmail.com>
2024-04-16 14:06:05 -0400

    Update index.js

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import authReducer from "./authReducer"
+import taskReducer from './taskReducers';
 
 const rootReducer = combineReducers({
   authReducer,
+  taskReducers
 });
 
 export default rootReducer;
